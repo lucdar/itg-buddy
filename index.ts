e34aa68ef05bcc58e1a29f157f851e89ccc9abe8
Lucas Clark <lucas.clark@berkeley.edu>
2023-05-16 19:20:49 -0700

    use GetCommands in index.ts

--- index.ts
+++ index.ts
@@ -3,27 +3,17 @@ const path = require('node:path');
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import { token } from './secrets.json';
 import { Command } from './utils/Interfaces';
+import { getCommands } from './utils/helperFunctions';
 import { ChatInputCommandInteraction } from 'discord.js';
 
 class CommandClient extends Client {
     commands: Collection<string, Command>;
     constructor() {
         super({ intents: [GatewayIntentBits.Guilds] });
-        this.commands = new Collection();
+        this.commands = getCommands();
     }
 }
 const client = new CommandClient();
-
-// Read all files in the commands directory and add them to the client.commands collection.
-// From the discord.js.guide tutorial, unsure if this is the best way to do this. Reading files is kind of jank tbh.
-// Weird how the fs functions are any type output. Maybe I have a configuration set up wrong?
-const commandsPath: string = path.join(__dirname, 'commands');
-const commandFiles: string[] = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command: Command = require(filePath);
-    client.commands.set(command.data.name, command);
-}
 
 // Execute the commands when they are called.
 client.on(Events.InteractionCreate, async interaction => {
