c0042e89e9ebd08ae0aef636e5b275d8811454b9
Lucas Clark <lucas.clark@berkeley.edu>
2023-06-06 20:34:51 -0700

    implement command lock

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits, CommandInteraction} from 'discord.js';
 import { token } from './secrets.json';
 import { Command } from './utils/Interfaces';
 import { getCommands } from './utils/helperFunctions';
@@ -15,19 +15,13 @@ class CommandClient extends Client {
 }
 const client = new CommandClient();
 
-// Execute the commands when they are called.
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	// console.log(interaction);
-
-    const command = client.commands.get(interaction.commandName);
-    if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-    try {
-		await command.execute(interaction);
+// CommandLock prevents the same command from being executed multiple times in parallel.
+const commandLock = new Set();
+async function executeCommand(command: Command, interaction: CommandInteraction) {
+	try {
+		await command.execute(interaction).then(() => {
+			commandLock.delete(command.data.name);
+		});
 	} catch (error) {
 		console.error(error);
 		const errorReply = 'There was an error while executing this command!'
@@ -36,6 +30,24 @@ client.on(Events.InteractionCreate, async interaction => {
 		} else {
 			await interaction.reply({ content: errorReply, ephemeral: true });
 		}
+		commandLock.delete(command.data.name);
+	}
+}
+
+// Execute the commands when they are called.
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+    const command = client.commands.get(interaction.commandName);
+    if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+	if (commandLock.has(command.data.name)) {
+		console.log(`Command ${command.data.name} is already running.`);
+		interaction.reply({ content: 'This command is already running. Please wait and try again.', ephemeral: true });
+	} else {
+		commandLock.add(command.data.name);
+		executeCommand(command, interaction);
 	}
 });
 
