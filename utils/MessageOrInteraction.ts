1ad12c186d11c2b8e011de598b032aee643d6568
Lucas Clark <lucas.clark@berkeley.edu>
2023-07-11 13:29:47 -0700

    support long messages in editReply

--- utils/MessageOrInteraction.ts
+++ utils/MessageOrInteraction.ts
@@ -45,14 +45,59 @@ export class MessageOrInteraction {
     }
   }
 
-  editReply(content: string | MessagePayload): Promise<Message<boolean>> {
+  async editReply(content: string | MessagePayload): Promise<Message<boolean>> {
+    // Split the message into multiple messages if it is too long.
+    const messages: string[] = generateMessages(content);
     if (this.interaction) {
-      return this.interaction.editReply(content);
-    } else {
-      if (!this.messageReply) {
-        throw new Error("MessageOrInteraction: messageReply is null.");
+      // Messages is empty if content is a MessagePayload
+      if (messages.length === 0) {
+        return this.interaction.editReply(content);
+      } else {
+        this.interaction.editReply(messages.pop() as string);
+        while (messages.length > 1) {
+          this.interaction.followUp(messages.pop() as string);
+        }
+        return this.interaction.followUp(messages.pop() as string);
       }
-      return this.messageReply.edit(content);
+    } else if (this.message && this.messageReply) {
+      if (messages.length === 0) {
+        return this.messageReply.edit(content);
+      } else {
+        this.messageReply.edit(messages.pop() as string);
+        while (messages.length > 1) {
+          await this.messageReply
+            .reply(messages.pop() as string)
+            .then((message) => {
+              this.messageReply = message;
+            });
+        }
+        return this.messageReply
+          .reply(messages.pop() as string)
+          .then((message) => {
+            this.messageReply = message;
+            return message;
+          });
+      }
     }
+    throw new Error("MessageOrInteraction: message and interaction are null.");
   }
+}
+
+function generateMessages(content: string | MessagePayload) {
+  const messages: string[] = [];
+  if (typeof content === "string") {
+    if (content.length > 2000) {
+      while (content.length > 2000) {
+        // Slice the content at newline boundaries when possible for cleaner messages.
+        let sliceIndex = content.lastIndexOf("\n", 2000);
+        if (sliceIndex === -1) {
+          sliceIndex = 2000;
+        }
+        messages.push(content.slice(0, sliceIndex));
+        content = content.slice(2000);
+      }
+    }
+    messages.push(content);
+  }
+  return messages;
 }
