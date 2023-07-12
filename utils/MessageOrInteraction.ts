53b33a98c69e70783582da5565a344f08f909733
Lucas Clark <lucas.clark@berkeley.edu>
2023-07-12 16:59:59 -0700

    fix bugs/styling with message splitting

--- utils/MessageOrInteraction.ts
+++ utils/MessageOrInteraction.ts
@@ -50,29 +50,29 @@ export class MessageOrInteraction {
     const messages: string[] = generateMessages(content);
     if (this.interaction) {
       // Messages is empty if content is a MessagePayload
-      if (messages.length === 0) {
+      if (messages.length === 0 || messages.length === 1) {
         return this.interaction.editReply(content);
       } else {
-        this.interaction.editReply(messages.pop() as string);
+        this.interaction.editReply(messages.shift() as string);
         while (messages.length > 1) {
-          this.interaction.followUp(messages.pop() as string);
+          await this.interaction.followUp(messages.shift() as string);
         }
-        return this.interaction.followUp(messages.pop() as string);
+        return this.interaction.followUp(messages.shift() as string);
       }
     } else if (this.message && this.messageReply) {
-      if (messages.length === 0) {
+      if (messages.length === 0 || messages.length === 1) {
         return this.messageReply.edit(content);
       } else {
-        this.messageReply.edit(messages.pop() as string);
+        this.messageReply.edit(messages.shift() as string);
         while (messages.length > 1) {
           await this.messageReply
-            .reply(messages.pop() as string)
+            .reply(messages.shift() as string)
             .then((message) => {
               this.messageReply = message;
             });
         }
         return this.messageReply
-          .reply(messages.pop() as string)
+          .reply(messages.shift() as string)
           .then((message) => {
             this.messageReply = message;
             return message;
@@ -85,16 +85,17 @@ export class MessageOrInteraction {
 
 function generateMessages(content: string | MessagePayload) {
   const messages: string[] = [];
+  const maxSlice = 2000;
   if (typeof content === "string") {
-    if (content.length > 2000) {
-      while (content.length > 2000) {
+    if (content.length > maxSlice) {
+      while (content.length > maxSlice) {
         // Slice the content at newline boundaries when possible for cleaner messages.
-        let sliceIndex = content.lastIndexOf("\n", 2000);
+        let sliceIndex = content.lastIndexOf("\n", maxSlice);
         if (sliceIndex === -1) {
-          sliceIndex = 2000;
+          sliceIndex = maxSlice;
         }
         messages.push(content.slice(0, sliceIndex));
-        content = content.slice(2000);
+        content = content.slice(sliceIndex);
       }
     }
     messages.push(content);
