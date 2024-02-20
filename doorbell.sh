b51e0e22b922a2a8fa4b702eeeab5a1ac103cf31
Dance Dance <dance@ddr.ocf.berkeley.edu>
2024-02-19 20:14:26 -0800

    fix quotation on doorbell script

--- doorbell.sh
+++ doorbell.sh
@@ -6,4 +6,4 @@ thirty_seconds=30000
 
 eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME xfce4-session)/environ)"
 
-notify-send -t $thirty_seconds "$name is at the door." $message
+notify-send -t $thirty_seconds "$name is at the door." "$message"
